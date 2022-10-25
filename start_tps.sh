18b77e5a70b06d2701e8826d0a0d5c98a2ae9b17
SKT-T1-otto <sktotto970@gmail.com>
2022-10-25 11:13:35 +0800

    optimize start_tps.sh

--- start_tps.sh
+++ start_tps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 d=$(date -R)
-exec 1>log/test_"$d".log
-build/./meter -config=build/config.json -group="$1" -user="$2" -last="$3"
+nohup ./build/meter -config=build/config.json -group="$1" -user="$2" -last="$3" >>log/test_"$d".log &
